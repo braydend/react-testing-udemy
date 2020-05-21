ff3f8f480f3140f854c4068d8f37543118cbd3c0
Brayden <braydendiluca@gmail.com>
2020-05-21 16:50:53 +1000

    Add click counter base files

---
+++ click-counter/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>The counter is currently: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
