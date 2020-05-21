1abb91bb3ed1c4ca0ac05b8eb5dce6b28888b480
Brayden <braydendiluca@gmail.com>
2020-05-21 17:37:02 +1000

    Add state tests

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -38,7 +38,26 @@ test('renders counter display', () => {
 });
 
 test('counter starts at 0', () => {
+  const wrapper = setUp();
+
+  const counter = findByTestAttr(wrapper, 'counter-display');
+  
+  // This does not work with functional components
+  // expect(wrapper.state('counter')).toBe(0);
+  expect(counter.text()).toContain('0');
 });
 
 test('clicking button increments counter display', () => {
+  const wrapper = setUp();
+
+  // If the counter is defined as follows, it does not update after the click
+  // const counter = findByTestAttr(wrapper, 'counter-display');
+  const getCounter = () => findByTestAttr(wrapper, 'counter-display');
+  const button = findByTestAttr(wrapper, 'increment-button');
+
+  expect(getCounter().text()).toContain('0');
+  button.simulate('click');
+  // This will return the value from when the wrapper was generated
+  // expect(counter.text()).toContain('1');
+  expect(getCounter().text()).toContain('1');
 });
