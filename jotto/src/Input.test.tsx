38c1074c8f99be9a33cfb53a015f32576d1e3b5f
Brayden <braydendiluca@gmail.com>
2020-06-15 17:07:37 +1000

    Add test for input change

--- jotto/src/Input.test.tsx
+++ jotto/src/Input.test.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { shallow } from "enzyme";
 import Input from "./Input";
 
-const setUp = () => shallow(<Input secretWord="party" />);
+const setUp = (secretWord: string = 'party') => shallow(<Input secretWord={secretWord} />);
 
 describe('<Input />', () => {
     test('renders without error', () => {
         const wrapper = setUp();
 
         expect(wrapper.length).toBe(1);
+    });
+
+    test('state updates when input is changed', () => {
+        const mockSetCurrentGuess = jest.fn();
+        React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+
+        const wrapper = setUp();
+        const input = wrapper.find('input');
+
+        const mockEvent = { target: { value: "foo" } };
+        input.simulate("change", mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith("foo");
     });
 });
--- jotto/src/Input.tsx
+++ jotto/src/Input.tsx
@@ -5,7 +5,19 @@ type Props = {
 };
 
 const Input: React.FC<Props> = ({ secretWord }) => {
-    return (<div></div>);
+    // useState is not destructured on import so it can be mocked
+    const [currentGuess, setCurrentGuess] = React.useState<string>();
+
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+    };
+
+    return (<div>
+        <form>
+            <input type="text" placeholder="Enter guess" onChange={({ target: { value } }) => setCurrentGuess(value)} value={currentGuess} />
+            <button onClick={handleSubmit}>Submit</button>
+        </form>
+    </div>);
 };
 
 export default Input;
